3b41f7d61ab226d69c596fc010f50f10a85a214d
Гильдина <70536744+emilytozier@users.noreply.github.com>
2026-01-08 23:51:26 +0400

    Update ElasticsearchConfig.java

--- src/main/java/com/example/logSleuthEnterprise/config/ElasticsearchConfig.java
+++ src/main/java/com/example/logSleuthEnterprise/config/ElasticsearchConfig.java
@@ -1,2 +1,22 @@
-package com.example.logSleuthEnterprise.config;public class ElasticsearchConfig {
+package com.example.logSleuthEnterprise.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+@Configuration
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.elasticsearch.uris}")
+    private String elasticsearchUris;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        // Удаляем "http://" если есть
+        String hostPort = elasticsearchUris.replace("http://", "").replace("https://", "");
+        return ClientConfiguration.builder()
+                .connectedTo(hostPort)
+                .build();
+    }
 }
